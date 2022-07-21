4c797b54944fd2d678ac1a2f8fb39b4143a91211
Po <po@Pos-MacBook-Pro.localdomain>
2022-07-21 12:37:13 -0600

    Added rollbar

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ rollbar.log('Hello world!')
 const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.get('/', (req, res) => {
+    rollbar.info("Someone loaded your HTML")
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
@@ -37,11 +38,14 @@ app.post('/api/students', (req, res) => {
 
    try {
        if (index === -1 && name !== '') {
+           rollbar.log("Student added successfully", {author: "PH", type: "manual entry"}); 
            students.push(name)
            res.status(200).send(students)
        } else if (name === ''){
+        rollbar.error("No name provided");
            res.status(400).send('You must enter a name.')
        } else {
+        rollbar.error("Student alredy exists");
            res.status(400).send('That student already exists.')
        }
    } catch (err) {
@@ -53,6 +57,7 @@ app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
     
     students.splice(targetIndex, 1)
+    rollbar.info("Student was deleted");
     res.status(200).send(students)
 })
 
